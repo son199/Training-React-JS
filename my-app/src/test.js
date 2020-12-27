dca2c6a89bc1e42be1d1f4dea3d7fc7d38b71465
Khai Phan Van <phan_van_khai123@yahoo.com>
2020-12-27 10:57:42 +0700

    new data fake

---
+++ my-app/src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test(props) {
+console.log(props);
+    return (
+        <div>
+        phan 1 {props.email} 
+        </div>
+    );
+}
+
+export default Test;
